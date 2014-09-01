77f89b17229d4eadeb2b680ef7e7e6321238d85d
Thomas Gerlach <s0531712@htw-berlin.de>
2014-09-01 12:51:41 +0200

    Execute added

---
+++ StromaDetectionExecute/Ausgabe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StromaDetectionExecute
+{
+  public class Ausgabe
+  {
+    public uint Identify;
+    public bool Result;
+  }
+}
---
+++ StromaDetectionExecute/Eingabe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace StromaDetectionExecute
+{
+  public class Eingabe
+  {
+    public uint Identify;
+    public string FileName;
+    public Point UpperLeft;
+    public Point LowerRight;
+  }
+}
